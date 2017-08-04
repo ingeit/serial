bceacb7fc6e8510837e66b3a8f300e0471fa749e
Ricky Bruno <ricardobruno_89@hotmail.com>
2017-08-04 19:42:27 -0300

    pruebas array mensaje

--- backend/controllers/serialController.js
+++ backend/controllers/serialController.js
@@ -7,12 +7,33 @@ exports.iniciar = function(socket){
 
     serialPort.on("open", function () {
         console.log('open');
+        var primerByte = 1;
+        var trama = [];
+        var tamTrama = 0;
         serialPort.on('data', function(data) {
-            var view   = new Int32Array(data);
-            socket.emit('message', view.toString());
-            console.log(view.toString());
+            console.log(data); 
+            // var datos = parseInt(new Int32Array(data).toString());
+            // if(primerByte === 1){
+            //     primerByte = 0;
+            //     if(datos === 0) tamTrama = 2;
+            //     else tamTrama = 21;
+            //     console.log("tamaño trama ",tamTrama);
+            //     trama.push(datos);
+            // }else{
+            //     trama.push(datos);
+            //     tamTrama = tamTrama - 1;
+            //     if(tamTrama === 0){
+            //         primerByte = 1;
+            //         console.log(trama);
+            //         trama = [];
+            //     }
+            // }
+            // socket.emit('message', view.toString());
+            // console.log(view.toString());
+            // console.log(data);
         });
     });
+
 
     socket.on('connection', function(connection) {
         console.log('User Connected');
