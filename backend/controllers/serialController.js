4896b6e6cf6a4e0d24b6f16ef5753bab7f06966b
MasterK <masterk@macpro.local>
2017-08-05 17:09:42 -0300

    fix?

--- backend/controllers/serialController.js
+++ backend/controllers/serialController.js
@@ -1,48 +1,55 @@
-var SerialPort = require("serialport"); 
-// var serialPort = new SerialPort('/dev/tty.usbserial', {
-var serialPort = new SerialPort('COM3', {
+const SerialPort = require('serialport');
+const ByteLength = SerialPort.parsers.ByteLength
+const port = new SerialPort('/dev/tty.usbserial',{
     baudRate: 115200,
 });
-
+const parser = port.pipe(new ByteLength({length: 3}));
 
 exports.iniciar = function(socket){
-
-    serialPort.on("open", function () {
+    port.on("open", function () {
         console.log('open');
         var primerByte = 1;
         var trama = [];
         var tamTrama = 0;
-        serialPort.on('data', function(byte) {
-            console.log(byte);            
-            if(primerByte === 1){
-                primerByte = 0;
-                if(byte === 0) tamTrama = 2;
-                else tamTrama = 21;
-                console.log("tamaño trama ",tamTrama);
-                trama.push(byte);
-            }else{
-                trama.push(byte);
-                tamTrama = tamTrama - 1;
-                if(tamTrama === 0){
-                    primerByte = 1;
-                    console.log(trama);
-                    trama = [];
-                }
-            }
-            socket.emit('message', view.toString());
-            console.log(view.toString());
-            console.log(data);
+
+        parser.on('data', function(data) {
+            var byte   = new Int32Array(data);
+            byte = byte.toString(); 
+            console.log('trama numero',primerByte)
+            console.log(byte)   
+            console.log(byte[0])
+            console.log(byte[2])  
+            console.log(byte[4])   
+            primerByte++;  
+            // if(primerByte === 1){
+            //     primerByte = 0;
+            //     if(byte === 0) tamTrama = 2;
+            //     else tamTrama = 21;
+            //     console.log("tamaño trama ",tamTrama);
+            //     trama.push(byte);
+            // }else{
+            //     trama.push(byte);
+            //     tamTrama = tamTrama - 1;
+            //     if(tamTrama === 0){
+            //         primerByte = 1;
+            //         console.log(trama);
+            //         trama = [];
+            //     }
+            // }
+            // socket.emit('message', view.toString());
+            // console.log(view.toString());
+            // console.log(data);
         });
     });
 
 
 
-    socket.on('connection', function(connection) {
-        console.log('User Connected');
-        connection.on('message', function(msg){
-            socket.emit('message', msg);
-        });
-    });
+    // socket.on('connection', function(connection) {
+    //     console.log('User Connected');
+    //     connection.on('message', function(msg){
+    //         socket.emit('message', msg);
+    //     });
+    // });
 }
 
 
