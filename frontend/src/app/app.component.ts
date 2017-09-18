18074db572abd82c83addea7ad4df9e9aabf4ec1
MasterK <masterk@macpro.local>
2017-09-18 14:54:42 -0300

    casi funcionando

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { EmbeddedSortableComponent } from '../pages/embedded-sortable-component/
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = EmbeddedSortableComponent;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
